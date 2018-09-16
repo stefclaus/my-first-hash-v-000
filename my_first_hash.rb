5c27248a2ecc751cb922f32a7b50b66dc42ad700
Stef Claus <stef.claus@gmail.com>
2018-09-16 20:57:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,13 +22,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"] = 2
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4 
+  shipping_manifest
 
 end
